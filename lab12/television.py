963a9e6414877bbe765c7960b12921bff6e959b4
isaiahschooluno@gmail.com <#7IA@582dECc^>
2024-07-01 09:02:50 -0500

    First commit

---
+++ lab12/television.py
@@ -0,0 +1,63 @@
+class Television:
+    MIN_VOLUME = 0
+    MAX_VOLUME = 2
+    MIN_CHANNEL = 0
+    MAX_CHANNEL = 3
+
+    def __init__(self, min_volume=MIN_VOLUME, max_volume=MAX_VOLUME, min_channel=MIN_CHANNEL, max_channel=MAX_CHANNEL):
+        self.__status = False
+        self.__muted = False
+        self.__volume = min_volume
+        self.__channel = min_channel
+
+    def power(self):
+        if self.__status:
+            self.__status = False
+        else:
+            self.__status = True
+
+    def mute(self):
+        if self.__status:
+            if self.__muted:
+                self.__muted = False
+            else:
+                self.__muted = True
+
+    def channel_up(self):
+        if self.__status:
+            if self.__channel == self.MAX_CHANNEL:
+                self.__channel = self.MIN_CHANNEL
+            else:
+                self.__channel += 1
+
+    def channel_down(self):
+        if self.__status:
+            if self.__channel == self.MIN_CHANNEL:
+                self.__channel = self.MAX_CHANNEL
+            else:
+                self.__channel -= 1
+
+    def volume_down(self):
+        if self.__muted:
+            self.__muted = False
+        if self.__status:
+            if self.__muted:
+                self.__muted = False
+            if self.__volume == self.MIN_VOLUME:
+                pass
+            else:
+                self.__volume -= 1
+
+    def volume_up(self):
+        if self.__muted:
+            self.__muted = False
+        if self.__status:
+            if self.__muted:
+                self.__muted = False
+            if self.__volume == self.MAX_VOLUME:
+                pass
+            else:
+                self.__volume += 1
+
+    def __str__(self):
+        return f'Power = [{self.__status}], Channel = [{self.__channel}], Volume = [{0 if self.__muted else self.__volume}]'
